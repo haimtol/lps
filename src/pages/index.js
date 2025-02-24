d25b7bc5828dfc09f5fa994382750273951f291b
haimtol <haim.tol@gmail.com>
2025-02-24 16:40:14 +0200

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+javascript
+Wrap
+Copy
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Landing Pages Home</h1>
+      <p>This is the default page.</p>
+    </div>
+  );
+}
