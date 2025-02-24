7679dd9a2b46b8806442461ad6fa69398eb94f2a
haimtol <haim.tol@gmail.com>
2025-02-24 17:19:25 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,5 @@
-const path = require("path");
-
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions;
-
-  const affiliate = {
-    id: "test123",
-    name: "Test Affiliate",
-    referralCode: "TEST123"
-  };
-
-  createPage({
-    path: `/affiliate-${affiliate.id}`,
-    component: path.resolve(`src/templates/landing-page.js`),
-    context: {
-      name: affiliate.name,
-      referralCode: affiliate.referralCode
-    }
-  });
+module.exports = {
+  siteMetadata: {
+    title: "Landing Pages",
+  },
 };
